84497be51fc0c63ebaae1f1fa48d056dc0ca5f6b
Marko <129887087+Marko010101@users.noreply.github.com>
2025-08-31 15:54:48 +0400

    fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Define __dirname for ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
-  plugins: [tailwindcss()],
   build: {
-    outDir: "dist",
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, "index.html"),
