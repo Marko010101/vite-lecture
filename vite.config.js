76cf153dc7940ec3da8964118129272d586ae7aa
Marko <129887087+Marko010101@users.noreply.github.com>
2025-08-31 15:52:54 +0400

    fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,11 @@ export default defineConfig({
   plugins: [tailwindcss()],
   build: {
     outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        dashboard: path.resolve(__dirname, "dashboard.html"),
+      },
+    },
   },
 });
